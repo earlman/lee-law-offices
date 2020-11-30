e99edac9dadbe61f3f88f1a7e45c26f6f67edba2
earlman <earl.mangulabnan@gmail.com>
2020-11-30 13:44:52 -0600

    add 'under construction'

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class="layout">
-    <Navbar />
-    <section class="hero is-medium is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1">
-            {{ pageTitle }}
-          </h1>
-          <h2 v-if="pageSubtitle" class="subtitle">
-            {{ pageSubtitle }}
-          </h2>
-        </div>
-      </div>
-    </section>
-    <main>
-      <slot />
-    </main>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          Created by <a href="chadcollins.net"><strong>Chad Collins</strong></a
-          >. Powered by <a href="https://gridsome.org/"><strong>Gridsome</strong></a
-          >.
-        </p>
-      </div>
-    </footer>
-  </div>
+	<div class="layout">
+		<!-- <Navbar /> -->
+		<section class="hero is-medium is-primary">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title is-1">Under Construction</h1>
+					<h2 v-if="pageSubtitle" class="subtitle">
+						Thanks for visiting leelawltd.com! Our site will be available shortly.
+					</h2>
+				</div>
+			</div>
+		</section>
+		<main>
+			<slot />
+		</main>
+		<footer class="footer"></footer>
+	</div>
 </template>
 
 <static-query>
@@ -39,8 +29,8 @@ query {
 <script>
 import Navbar from '../components/Navbar';
 export default {
-  props: { pageTitle: '', pageSubtitle: '', color: '' },
-  components: { Navbar },
+	props: { pageTitle: '', pageSubtitle: '', color: '' },
+	components: { Navbar },
 };
 </script>
 
@@ -48,10 +38,11 @@ export default {
 @import '@/assets/scss/overrides.scss';
 
 body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
-    Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
+	font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+		Arial, sans-serif;
+	margin: 0;
+	padding: 0;
+	line-height: 1.5;
+	height: 100%;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,74 +1,5 @@
 <template>
-  <Layout pageTitle="Starter Blog" pageSubtitle="Gridsome / Buefy / Netlify CMS">
-    <div class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column"></div>
-          <div class="column cards-grid is-four-fifths">
-            <article class="card grid-item" v-for="edge in $page.posts.edges" :key="edge.node.id">
-              <div class="card-image">
-                <g-link :to="edge.node.path">
-                  <figure class="image is-16by9">
-                    <img
-                      :src="`../../${edge.node.featuredImage}`"
-                      :alt="`${edge.node.title} image`"
-                    />
-                  </figure>
-                </g-link>
-              </div>
-
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <g-link :to="edge.node.author.path">
-                      <figure class="image is-48x48">
-                        <img :src="`../../${edge.node.author.image}`" alt="Placeholder image" />
-                      </figure>
-                    </g-link>
-                  </div>
-                  <div class="media-content">
-                    <h2 class="title is-4">
-                      <g-link :to="edge.node.path">
-                        {{ edge.node.title }}
-                      </g-link>
-                    </h2>
-                    <p class="subtitle is-6">
-                      <g-link :to="edge.node.author.path">
-                        {{ edge.node.author.title }}
-                      </g-link>
-                    </p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  {{ edge.node.excerpt }}
-                </div>
-
-                <div>
-                  <span v-for="(tag, index) in edge.node.tags" :key="tag.id">
-                    <div class="tag">
-                      <g-link :to="tag.path">
-                        {{ tag.id }}
-                      </g-link>
-                    </div>
-
-                    <!-- We will add a comma separator for the tags -->
-                    <span v-if="index + 1 < edge.node.tags.length">
-                      ,
-                    </span>
-                  </span>
-                </div>
-              </div>
-            </article>
-
-            <div class="grid-item"></div>
-            <div class="grid-item"></div>
-          </div>
-          <div class="column"></div>
-        </div>
-      </div>
-    </div>
-  </Layout>
+	<Layout pageTitle="Starter Blog" pageSubtitle="Gridsome / Buefy / Netlify CMS"> </Layout>
 </template>
 
 <page-query>
@@ -99,41 +30,41 @@
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!',
-  },
+	metaInfo: {
+		title: 'Home',
+	},
 };
 </script>
 
 <style>
 .card {
-  margin-bottom: 1.5rem;
+	margin-bottom: 1.5rem;
 }
 
 .cards-grid {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  list-style: none;
-  width: 100%;
-  justify-content: center;
+	display: flex;
+	flex-wrap: wrap;
+	gap: 20px;
+	list-style: none;
+	width: 100%;
+	justify-content: center;
 }
 
 /* Mobile First */
 .grid-item {
-  width: 100%;
-  transition: all 0.2s ease-in-out;
+	width: 100%;
+	transition: all 0.2s ease-in-out;
 }
 /*Medium Width */
 @media screen and (min-width: 740px) {
-  .grid-item {
-    width: calc((100% / 2) - 30px);
-  }
+	.grid-item {
+		width: calc((100% / 2) - 30px);
+	}
 }
 /*Wide Width */
 @media screen and (min-width: 991px) {
-  .grid-item {
-    width: calc((100% / 3) - 30px);
-  }
+	.grid-item {
+		width: calc((100% / 3) - 30px);
+	}
 }
 </style>

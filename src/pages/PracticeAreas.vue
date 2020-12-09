8e24c793eab26d9992c7754b4dd6edd2f605b97e
earlman <earl.mangulabnan@gmail.com>
2020-12-09 15:53:33 -0600

    add content to practice-areas

--- src/pages/PracticeAreas.vue
+++ src/pages/PracticeAreas.vue
@@ -1,5 +1,200 @@
 <template>
   <Layout >
+
+      <div class="content">
+
+<p class=MsoNormal>Practice Areas</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Harassment � Enough Is Enough!</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Harassment can be verbal or physical.<span
+style='mso-spacerun:yes'>� </span>Your harasser can be your co-worker and
+supervisor, or even a non-employee like a customer or contractor.<span
+style='mso-spacerun:yes'>� </span>If you have ever experienced any of the following,
+you are being harassed: </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>�Mexicans are rapists.� </p>
+
+<p class=MsoNormal>�Blacks are lazy.� </p>
+
+<p class=MsoNormal>�Why do you keep getting pregnant? Control yourself.�</p>
+
+<p class=MsoNormal><span style='mso-spacerun:yes'>�</span>�Go back to your country.�</p>
+
+<p class=MsoNormal>�Open your slanted eyes.� <span
+style='mso-spacerun:yes'>�</span></p>
+
+<p class=MsoNormal>�You�re too old to get this job done.�</p>
+
+<p class=MsoNormal>�I can�t understand your accent.� </p>
+
+<p class=MsoNormal>Your supervisor/co-worker touches your body parts behind
+you.</p>
+
+<p class=MsoNormal>Your supervisor/co-worker constantly stares at your body parts.
+</p>
+
+<p class=MsoNormal>Your supervisor/co-worker removes or tries to remove your hijab.
+</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Enough is enough. It is time to stand up against your
+harasser. We are here to protect you. No legal fees unless we win for you. Contact
+us for a free consultation.</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Discrimination � Stop Getting Screwed Over!</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>The law protects you from being discriminated based on race,
+sex/gender, sexual orientation, age, religion, pregnancy, and disability.<span
+style='mso-spacerun:yes'>� </span>If you have ever experienced any of the
+following, you are being discriminated: </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Your promotion was denied after you became pregnant and your
+supervisor says, �The company needs someone who will be more dedicated to the
+position.�</p>
+
+<p class=MsoNormal>Despite having more experience and qualifications than other
+applicants, you are passed over for a senior position because of your age, sex,
+or religious belief.</p>
+
+<p class=MsoNormal>Your hours were cut down after you asked for time-off for
+your medical treatment.</p>
+
+<p class=MsoNormal>You recently found out your co-workers earn more than you
+despite having the same experience and qualifications.</p>
+
+<p class=MsoNormal>You are being excluded or isolated by your co-workers. For
+instance, after your co-workers recently found out you are gay, they started
+calling you �princess�.</p>
+
+<p class=MsoNormal>You are given impossible tasks no matter how hard you try.</p>
+
+<p class=MsoNormal>Your supervisor deliberately withholds information you need
+to do your job. <span style='mso-spacerun:yes'>�</span></p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Enough is enough. It is time to stand up for your rights. We
+are here to protect you. No legal fees unless we win for you. Contact us for a
+free consultation.</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Wage &amp; Hour � Claim What You Have Earned!</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Minimum Wage: As of January 1, 2020, the minimum wage in
+California is $12.00 per hour for employers with twenty-five or fewer, and $13.00
+per hour for employers with twenty-six (26) or more. That minimum wage is
+scheduled to rise annually until it reaches $15 per hour for all employers in
+2022. Please refer to the following link for the minimum wage for workers in
+the unincorporated areas of Los Angeles County. <span
+style='mso-spacerun:yes'>�</span>https://lacounty.gov/minimum-wage/</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Overtime: <span style='mso-spacerun:yes'>�</span></p>
+
+<p class=MsoNormal>Time &amp; A Half Overtime: You must get paid �one and a
+half times� your regular wage for any work either (<span class=SpellE>i</span>)
+exceeding eight (8) hours in one workday or (ii) exceeding forty (40) hours in
+one workweek. For example, your hourly wage is $20. If you work 10 hours in a
+workday, your wage of that day is $220 as follows: $20 x 8 hours ($160 � regular
+wage) plus $20 x 2 hours x 1.5 ($60 � overtime wage). </p>
+
+<p class=MsoNormal>Double Overtime: In addition, you must be paid �twice� your regular
+wage for any work exceeding twelve (12) hours in one workday, or any work exceeding
+eight (8) hours on the seventh day of a workweek. </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Meal Break: You must be given a meal break of at least
+thirty (30) minutes after working five (5) hours, and another thirty (30) minute
+meal break after working additional five (5) hours in a workday. Meal breaks
+are your time completely away from work and should be completely uninterrupted.
+You have earned every minute of your meal breaks. </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Rest Period: You mut be given ten (10) minutes of rest period
+for each four (4) hours.<span style='mso-spacerun:yes'>� </span>The �10-minutes�
+period is your time to rest from work. You have earned every minute of your
+rest periods. <span style='mso-spacerun:yes'>�</span></p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Wage Statement: Do you get your wage statements for each pay
+period? Do your wages statements show your rate of pay, hours worked, regular wage,
+overtime wage, sick time etc.? </p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Timecard: Who makes your timecard? Have you ever seen your
+timecard? Are you forced to sign your timecard? <span
+style='mso-spacerun:yes'>�</span><span style='mso-spacerun:yes'>�</span></p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Misclassification: Are you (mis)classified as an exempt employee
+and not getting overtime wages? Are you (mis)classified as an independent contractor
+and not getting any benefits?</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>It is never too late to claim what you have earned. We will
+deliver what belongs to you. No legal fees unless we win for you. Contact us
+for a free consultation.</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>Wrongful Termination � Stand Up <span class=GramE>To</span> Protect
+Yourself</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>If you ever experienced any of the following, you were wrongfully
+terminated from your work and are entitled to your lost wages:</p>
+
+<p class=MsoNormal>Did you get fired after asking for overtime wages or asking to
+correct your hours worked?</p>
+
+<p class=MsoNormal>Did you get fired after you became pregnant?</p>
+
+<p class=MsoNormal>Did you get fired after you reported harassment or discrimination
+against you or your co-worker to your supervisor?</p>
+
+<p class=MsoNormal>Did you get fired after you filed your worker�s compensation
+claim or reported injury at work?</p>
+
+<p class=MsoNormal>Did you get fired after you took family or medical leave?</p>
+
+<p class=MsoNormal>Did you get fired after you reported suspected criminal
+activity by your employer to a government or law enforcement agency?</p>
+
+<p class=MsoNormal>Did you have no choice but to quit because you could no
+longer tolerate harassment or discrimination at work?</p>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+<p class=MsoNormal>You deserve better. It is time to claim your lost wages. We
+are here to protect you. No legal fees unless we win for you. Contact us for a
+free consultation.</p>
+      </div>
+
   </Layout>
 </template>
 

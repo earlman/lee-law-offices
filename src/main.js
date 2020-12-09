5f20c45733478df011a9cbba0d333f579d2a3028
earlman <earl.mangulabnan@gmail.com>
2020-12-08 20:23:39 -0600

    add roboto font

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import DefaultLayout from '~/layouts/Default.vue';
 // Import Design System
 import '~/styles/index.sass'
 
+// Import Fonts
+import "fontsource-roboto"
+
 export default function (Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.component('Layout', DefaultLayout);

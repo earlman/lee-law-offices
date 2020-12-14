fa6f64021c6615cf3080a96c295cd8790179f14f
earlman <earl.mangulabnan@gmail.com>
2020-12-14 04:45:57 -0600

    hero updates

---
+++ src/components/IndexSections/Hero.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="hero-container">
+		<div class="hero">
+			<div class="content">
+				<h1 class="hero--title">
+					The law is on your side.<br />
+					We are here to protect you.
+				</h1>
+				<p class="hero--text">
+					Call us today for a free consultation! <br />
+					630-444-5656. Se habla español.
+				</p>
+				<button class="contact-us">Contact Us</button>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="sass" scoped>
+@import '@/styles/04 - Layout/_media.sass'
+
+.hero-container
+    background-image: url(~@/assets/images/hero.png)
+    background-position: 50% 0
+    height: min-content
+    background-repeat: no-repeat
+    background-size: auto 100%
+    padding: var(--space-sm)
+    display: flex
+    flex-direction: column
+
+    @include landscape
+        background-size: 100% auto
+
+    .hero
+        margin: auto
+
+        .content
+            border-radius: 3px
+            padding: var(--space-sm)
+            background-color: var(--color-b-alt-2)
+
+            h1, p
+                margin-bottom: var(--space-sm)
+            button
+                margin: auto
+
+// .content
+//     margin: auto
+//     background-color: var(--color-b-alt-2)
+//     padding: var(--space-sm)
+//     // opacity: .6
+
+//     @include md
+//         padding: var(--space-md)
+//         min-width: 300px
+//         max-width: 800px
+//         grid-row: 2
+
+button.contact-us
+    width: max-content
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,7 @@
 <template>
 	<Layout pageTitle="Home" pageSubtitle="">
-		<section class="hero-container">
-			<div class="hero">
-				<div class="content">
-					<h1 class="hero--title">
-						The law is on your side.<br />
-						We are here to protect you.
-					</h1>
-					<p class="hero--text">
-						Call us today for a free consultation! <br />
-						630-444-5656. Se habla español.
-					</p>
-					<button class="contact-us">Contact Us</button>
-				</div>
-			</div>
+		<section>
+			<hero />
 		</section>
 		<section>
 			<practice-areas-grid />
@@ -32,8 +20,10 @@
 import PracticeAreasGrid from '../components/IndexSections/PracticeAreasGrid.vue';
 import FreeConsultation from '../components/IndexSections/FreeConsultation.vue';
 import ContactUs from '../components/IndexSections/ContactUs.vue';
+import Hero from '../components/IndexSections/Hero.vue';
+
 export default {
-	components: { PracticeAreasGrid, FreeConsultation, ContactUs },
+	components: { PracticeAreasGrid, FreeConsultation, ContactUs, Hero },
 	metaInfo: {
 		title: 'Home',
 	},
@@ -48,38 +38,4 @@ export default {
 
 section
     margin-bottom: var(--space-lg)
-
-@include md
-    .hero-container
-        background-image: url(~@/assets/images/hero.png)
-        background-position: 100% 100%
-        min-height: 70vh
-        background-repeat: no-repeat
-        background-size: auto 100%
-
-.hero
-    display: grid
-    grid-template-rows: 1fr 3fr 1fr
-    max-width: var(--layout-max-width)
-
-    .hero--text
-        color: var(--color-t-c)
-
-.content
-    margin: auto
-    background-color: var(--color-b-alt-2)
-    padding: var(--space-sm)
-    // opacity: .6
-
-    h1
-        margin-bottom: var(--space-sm)
-
-    @include md
-        padding: var(--space-md)
-        min-width: 300px
-        max-width: 800px
-        grid-row: 2
-
-button.contact-us
-    width: max-content
 </style>

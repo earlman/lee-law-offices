4998de595d796d72fb183a7ced140c12e740240e
earlman <earl.mangulabnan@gmail.com>
2020-12-22 08:05:35 -0600

    disclaimer page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,59 @@
 <template>
-	<div></div>
+	<Layout title="Disclaimer">
+		<div class="disclaimer--container">
+			<h1 class="title">Disclaimer</h1>
+			<div class="disclaimer">
+				<div v-html="$page.pageContent.content"></div>
+			</div>
+		</div>
+	</Layout>
 </template>
 
 <script>
-export default {};
+export default {
+	components: {},
+};
 </script>
 
+<page-query>
+query {
+    pageContent(id: "Disclaimer"){
+        content
+    }
+}
+</page-query>
+
 <style lang="sass" scoped>
+@import '@/styles/04 - Layout/_media.sass'
+
+.disclaimer--container
+    margin: auto
+    @include xl
+        width: 1200px
+
+.title
+    background-color: var(--color-b-alt)
+    padding: var(--space-xs)
+    margin: var(--space-xs) var(--space-xs)
+    max-width: min-content
+
+    @include md
+        margin: var(--space-sm)
+        padding: var(--space-sm)
+
+.disclaimer
+    background-color: var(--color-b-alt)
+    padding: var(--space-xs)
+    margin: 0 var(--space-xs)
+
+    @include md
+        margin: var(--space-sm)
+        padding: var(--space-sm)
+
+    @include lg
+        width: max-content
+
+    ::v-deep h2
+        margin-top: var(--space-sm)
+        margin-bottom: var(--space-xs)
 </style>

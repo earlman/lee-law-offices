75352d1aa51bf2c8dec2f324227120c2ef87de7c
earlman <earl.mangulabnan@gmail.com>
2021-02-02 01:17:29 -0600

    index styles

--- src/components/IndexSections/BusinessLit.vue
+++ src/components/IndexSections/BusinessLit.vue
@@ -1,13 +1,34 @@
 <template>
 	<div class="businesslit responsive-container">
-		<h2>Business and Commercial Litigation</h2>
-		<p>
-			Our Business & Commercial Litigation Practice includes breach of contract, breach of
-			fiduciary duty and/or loyalty, partnership disputes, misappropriation of business
-			secrets, misrepresentation, fraud, interference with contract, and interference with
-			business operation. Our attorneys provide comprehensive advice in corporate law.
-		</p>
-		<g-link class="button" to="/employers">Read More</g-link>
+		<div class="title">
+			<h2>Business & Commercial Litigation</h2>
+		</div>
+		<div class="content">
+			<div class="content--text">
+				<p>
+					Our Business & Commercial Litigation Practice includes breach of contract,
+					breach of fiduciary duty and/or loyalty, partnership disputes, misappropriation
+					of business secrets, misrepresentation, fraud, interference with contract, and
+					interference with business operation. Our attorneys provide comprehensive advice
+					in corporate law.
+				</p>
+			</div>
+			<g-link class="button" to="/employers">
+				<span>Read More</span>
+				<svg
+					width="29"
+					height="29"
+					viewBox="0 0 29 29"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M19.3456 13.2917H4.8335V15.7084H19.3456V19.3334L24.1668 14.5001L19.3456 9.66675V13.2917V13.2917Z"
+						fill="white"
+					/>
+				</svg>
+			</g-link>
+		</div>
 	</div>
 </template>
 
@@ -15,5 +36,40 @@
 export default {};
 </script>
 
-<style lang="scss" scoped>
+<style lang="sass" scoped>
+@import '@/styles/04 - Layout/_media.sass'
+
+.title
+    margin-bottom: var(--space-sm)
+
+.content
+    align-items: start
+
+    p
+        grid-column: 1
+        margin-bottom: var(--space-sm)
+
+    @include lg
+        display: grid
+        grid-template-columns: 3fr 1fr
+        grid-gap: var(--space-md)
+
+.buttons
+    display: flex
+    flex-wrap: wrap
+    margin-top: var(--space-sm)
+
+    @include lg
+        margin-top: 0
+
+    .button
+        width: 100%
+        min-width: max-content
+        margin-bottom: var(--space-xs)
+
+        @include md
+            margin-right: var(--space-sm)
+
+        @include lg
+            margin-right: 0
 </style>
--- src/components/IndexSections/LaborLaw.vue
+++ src/components/IndexSections/LaborLaw.vue
@@ -1,16 +1,55 @@
 <template>
 	<div class="laborlaw responsive-container">
-		<h2>Labor and Employment Law</h2>
-		<p>
-			Our Labor & Employment Law Practice covers wage and hour claims, wrongful termination,
-			discrimination, and harassment. We have represented a variety of clients in class
-			actions as well as individual claims. Our practice is not limited to employees (usually
-			as plaintiffs) or employers (usually as defendants). We represent both sides. Having
-			represented both sides of the case, we know how to best serve our clients to meet their
-			unique needs.
-		</p>
-		<g-link class="button" to="/employees">For Employees</g-link>
-		<g-link class="button" to="/emplyers">For Employers</g-link>
+		<div class="title">
+			<h2>Labor & Employment Law</h2>
+		</div>
+		<div class="content">
+			<div class="content--text">
+				<p>
+					Our Labor & Employment Law Practice covers wage and hour claims, wrongful
+					termination, discrimination, and harassment. We have represented a variety of
+					clients in class actions as well as individual claims. Our practice is not
+					limited to employees (usually as plaintiffs) or employers (usually as
+					defendants).
+				</p>
+				<p>
+					We represent both sides. Having represented both sides of the case, we know how
+					to best serve our clients to meet their unique needs.
+				</p>
+			</div>
+			<div class="buttons">
+				<g-link class="button ba" to="/employees">
+					<span>For Employees</span>
+					<svg
+						width="29"
+						height="29"
+						viewBox="0 0 29 29"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M19.3456 13.2917H4.8335V15.7084H19.3456V19.3334L24.1668 14.5001L19.3456 9.66675V13.2917V13.2917Z"
+							fill="white"
+						/>
+					</svg>
+				</g-link>
+				<g-link class="button" to="/employers">
+					<span>For Employers</span>
+					<svg
+						width="29"
+						height="29"
+						viewBox="0 0 29 29"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M19.3456 13.2917H4.8335V15.7084H19.3456V19.3334L24.1668 14.5001L19.3456 9.66675V13.2917V13.2917Z"
+							fill="white"
+						/>
+					</svg>
+				</g-link>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -18,5 +57,42 @@
 export default {};
 </script>
 
-<style lang="scss" scoped>
+<style lang="sass" scoped>
+@import '@/styles/04 - Layout/_media.sass'
+
+.title
+    margin-bottom: var(--space-sm)
+
+.content
+    align-items: start
+
+    p
+        grid-column: 1
+
+    @include lg
+        display: grid
+        grid-template-columns: 3fr 1fr
+        grid-gap: var(--space-md)
+
+.buttons
+    display: flex
+    flex-wrap: wrap
+    margin-top: var(--space-sm)
+
+    @include lg
+        margin-top: 0
+
+    .button
+        width: 100%
+        min-width: max-content
+
+    .ba
+        margin-bottom: var(--space-xs)
+
+        @include md
+            margin-bottom: 0
+            margin-right: var(--space-2xs)
+
+        @include lg
+            margin-right: 0
 </style>

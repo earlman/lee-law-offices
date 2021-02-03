1774a5ac34f867ceb6d4bf69782d68f2ce4f1e82
earlman <earl.mangulabnan@gmail.com>
2021-02-03 15:39:41 -0600

    business litigation page

--- src/pages/BusinessLitigation.vue
+++ src/pages/BusinessLitigation.vue
@@ -7,6 +7,12 @@
 	>
 		<div class="content">
 			<div v-html="$page.pageContent.content" class="content--text"></div>
+			<div class="content--allegations">
+				<h4>Example Allegations:</h4>
+				<ul>
+					<li v-for="(item, i) in $page.pageContent.allegations" :key="i">{{ item }}</li>
+				</ul>
+			</div>
 		</div>
 	</PageLayout>
 </template>
@@ -45,12 +51,32 @@ export default {
 .page-buslit
     ::v-deep h4
         font-family: var(--font-secondary)
+        margin-top: 0
 
     .content
+
         @include lg
             display: grid
-            grid-template-columns: 3fr 1fr
+            grid-template-columns: 2fr 1fr
+            grid-gap: var(--space-sm)
 
         &--text
             background-color: white
+
+        &--allegations
+            background-color: var(--color-p)
+            padding-left: var(--space-md)
+            padding-right: var(--space-md)
+            padding-top: var(--space-md)
+            padding-bottom: var(--space-md)
+
+            h4
+                margin-top: 0
+                margin-bottom: var(--space-2xs)
+
+            li
+                margin-left: var(--space-sm)
+
+            *
+                color: white
 </style>

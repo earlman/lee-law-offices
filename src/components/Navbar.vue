62eaef43fed96c66a1f23dd6dfc3f12a9c8f3acd
earlman <earl.mangulabnan@gmail.com>
2021-02-03 06:35:51 -0600

    update navigation bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -33,7 +33,8 @@
 				<ul class="lg-nav">
 					<li><g-link to="/">Home</g-link></li>
 					<li><g-link to="/about">About</g-link></li>
-					<li><g-link to="/practice-areas">Practice Areas</g-link></li>
+					<li><g-link to="/employers">Employers</g-link></li>
+					<li><g-link to="/employees">Employees</g-link></li>
 					<li><g-link to="/contact-us">Contact Us</g-link></li>
 					<!-- <li><g-link  to="/">En Espanol</g-link></li> -->
 				</ul>

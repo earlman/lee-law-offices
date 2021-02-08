b57250d6b88b16ca4ba5ac2150bd99d33b6f51db
earlman <earl.mangulabnan@gmail.com>
2021-02-08 05:55:54 -0600

    add about me pages

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,60 +1,148 @@
 <template>
-	<Layout title="About">
-		<div class="disclaimer--container">
-			<h1 class="title">About</h1>
-			<div class="disclaimer">
-				<p>This page is under construction</p>
-				<!-- <div v-html="$page.pageContent.content"></div> -->
+	<PageLayout title="Who We Are" :bgImage="bgImage">
+		<template slot="sidebar">
+			<nav class="people--menu">
+				<ul>
+					<li v-for="edge in $page.people.edges" :key="edge.node.title">
+						<a :href="'#' + edge.node.link">
+							{{ edge.node.name }}
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</template>
+		<div class="people">
+			<div
+				v-for="edge in $page.people.edges"
+				:key="edge.node.title"
+				:class="edge.node.title"
+				class="person"
+				:id="edge.node.link"
+			>
+				<div class="person--head">
+					<h2 class="person--title">{{ edge.node.name }}</h2>
+				</div>
+				<div class="person--awards">
+					<h3>Awards:</h3>
+					<ul>
+						<li v-for="award in edge.node.awards">{{ award }}</li>
+					</ul>
+				</div>
+				<div class="person--education">
+					<h3>Education:</h3>
+					<ul>
+						<li v-for="e in edge.node.education">{{ e }}</li>
+					</ul>
+				</div>
+				<div class="person--admitted">
+					<h3>Admitted:</h3>
+					<ul>
+						<li v-for="a in edge.node.admitted">{{ a }}</li>
+					</ul>
+				</div>
+				<div class="person--content" v-html="edge.node.content" />
 			</div>
 		</div>
-	</Layout>
+	</PageLayout>
 </template>
 
-<script>
-export default {
-	components: {},
-};
-</script>
-
 <page-query>
-query {
-    pageContent(id: "Disclaimer"){
-        content
+query {  
+    people: allPeople(sortBy: "id", order: DESC) {
+      edges {
+            node {
+                name
+                title
+                education
+                admitted
+                awards
+                content
+            }
+        }
     }
 }
 </page-query>
 
+<script>
+import PageLayout from '@/layouts/Page';
+import img from '@/assets/images/practiceareas.png';
+
+export default {
+	data() {
+		return {
+			bgImage: img,
+		};
+	},
+	metaInfo: {
+		title: 'Practice Areas',
+	},
+	components: {
+		PageLayout,
+	},
+};
+</script>
+
 <style lang="sass" scoped>
 @import '@/styles/04 - Layout/_media.sass'
 
-.disclaimer--container
-    margin: auto
-    @include xl
-        width: 1200px
+.people
+    display: flex
+    flex-direction: column
 
-.title
-    background-color: var(--color-b-alt)
-    padding: var(--space-xs)
-    margin: var(--space-xs) var(--space-xs)
-    max-width: min-content
+    ::v-deep
+        //fix bullet points in practice-area on mobile
+        list-style-position: inside
 
-    @include md
-        margin: var(--space-sm)
-        padding: var(--space-sm)
+        @include md
+            li
+                margin-left: var(--space-md)
+                list-style-position: outside
+                width: 80%
 
-.disclaimer
-    background-color: var(--color-b-alt)
-    padding: var(--space-xs)
-    margin: 0 var(--space-xs)
+        //colored background in some parts
+        aside
+            background-color: var(--color-p)
+            border-radius: 3px
+            padding-top: var(--space-sm)
+            padding-bottom: var(--space-sm)
+            padding-left: var(--space-xs)
+            padding-right: var(--space-xs)
+            margin-top: var(--space-md)
+            margin-left: calc(-1*var(--space-xs))
+            margin-right: calc(-1*var(--space-xs))
+            margin-bottom: calc(-1*var(--space-xs))
 
-    @include md
-        margin: var(--space-sm)
-        padding: var(--space-sm)
+            @include md
+                margin-left: calc(-1*var(--space-md))
+                margin-right: calc(-1*var(--space-md))
+                margin-bottom: calc(-1*var(--space-md))
 
-    @include lg
-        width: max-content
+            ul
+                margin-bottom: 0
 
-    ::v-deep h2
-        margin-top: var(--space-sm)
+                @include lg
+                    column-count: 2
+
+                li
+                    margin-bottom: var(--space-2xs)
+            *
+                color: white
+
+    .area
+        background-color: var(--color-b-alt)
+
+    .area
+        padding: var(--space-xs)
+
+        @include md
+            padding: var(--space-md)
+
+    .area--title
+        // margin-top: var(--space-xs)
         margin-bottom: var(--space-xs)
+
+.people
+
+    & > *
+        margin-bottom: var(--space-md)
 </style>

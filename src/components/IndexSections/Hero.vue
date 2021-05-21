2a270da2956b7e57fb807d62a53541da708f380f
Earl Man <earl.mangulabnan@gmail.com>
2021-05-20 19:42:02 -0500

    formatting

--- src/components/IndexSections/Hero.vue
+++ src/components/IndexSections/Hero.vue
@@ -31,73 +31,73 @@ export default {
 @import '@/styles/04 - Layout/_media.sass'
 
 .hero-container
-    background-position: 0 20%
-    background-image: url(~@/assets/images/landing-mobile.webp)
+   background-position: 0 20%
+   background-image: url(~@/assets/images/landing-mobile.webp)
     // min-height: 500px
-    background-repeat: no-repeat
-    background-size: cover
-    padding: 0 var(--space-sm)
-    display: flex
-    flex-direction: column
-    min-height: 90vh
-    width: 100vw
-    box-shadow: inset 0px 0px 4px 4px rgba(80, 131, 119, 0.2)
+   background-repeat: no-repeat
+   background-size: cover
+   padding: 0 var(--space-sm)
+   display: flex
+   flex-direction: column
+   min-height: 90vh
+   width: 100vw
+   box-shadow: inset 0px 0px 4px 4px rgba(80, 131, 119, 0.2)
 
-    @include md
-        background-position: top center
-        background-image: url(~@/assets/images/landing-hero.webp)
-        min-height: 50vh
+   @include md
+      background-position: top center
+      background-image: url(~@/assets/images/landing-hero.webp)
+      min-height: 50vh
 
-    @include lg
-        padding: var(--space-md) var(--space-sm) calc(1.1 * var(--space-2xl))
-        margin: auto
+   @include lg
+      padding: var(--space-md) var(--space-sm) calc(1.1 * var(--space-2xl))
+      margin: auto
 
-    @include xl
-        background-position: center center
+   @include xl
+      background-position: center center
 
-    .hero
-        margin: var(--space-sm) auto
-        width: 100%
-        max-width: var(--layout-max-width)
+   .hero
+      margin: var(--space-sm) auto
+      width: 100%
+      max-width: var(--layout-max-width)
 
-        &--title
-            color: var(--color-t-h)
-            font-size: 20px
-            margin-bottom: var(--space-xs)
-            font-family: var(--font-secondary)
+      &--title
+         color: var(--color-t-h)
+         font-size: 20px
+         margin-bottom: var(--space-xs)
+         font-family: var(--font-secondary)
 
-            @include md
-                font-size: var(--d-xl)
+         @include md
+            font-size: var(--d-xl)
 
             @include lg
-                font-size: var(--d-2xl)
+               font-size: var(--d-2xl)
 
-        &--text
+      &--text
 
-            p, em
-                color: var(--color-t-c)
-                margin-bottom: var(--space-2xs)
+         p, em
+            color: var(--color-t-c)
+            margin-bottom: var(--space-2xs)
 
-        .content
+      .content
+         padding: var(--space-sm)
+         // background-color: var(--color-b-alt)
+         // margin: auto
+         background-color: #faf6f5
+         background-image: url(~@/assets/images/background-tile-fabric.png)
+         max-width: max-content
+         box-shadow: #959da533 0px 8px 24px
+
+         @include md
+            margin: unset
             padding: var(--space-sm)
-            // background-color: var(--color-b-alt)
-            // margin: auto
-            background-color: #faf6f5
-            background-image: url(~@/assets/images/background-tile-fabric.png)
-            max-width: max-content
-            box-shadow: #959da533 0px 8px 24px
 
-            @include md
-                margin: unset
-                padding: var(--space-sm)
-
-            @include lg
-                padding: var(--space-md)
-                margin: 0
+         @include lg
+            padding: var(--space-md)
+            margin: 0
 
 button.contact-us
-    width: 100%
-    margin-top: var(--space-xs)
-    @include md
-        width: max-content
+   width: 100%
+   margin-top: var(--space-xs)
+   @include md
+      width: max-content
 </style>
